f56f41869bc9c91abcf1e9e22a9cc17d4ac50735
Matt Turner <mattturner1911@gmail.com>
2022-06-02 23:47:50 -0400

    Completed exercise 1-6

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+    let newString = "";
 
+    if (num < 0) {
+        return "ERROR";
+    }
+
+    for (let index = 0; index < num; index++) {
+        newString = newString + string;
+    }
+
+    return newString;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let reverseString = str.split("").reverse().join("");
 
+    return reverseString;
 };
 
 // Do not edit below this line
